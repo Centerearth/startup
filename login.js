e42404f44ed2d6631416b106a6470daa9eea899d
Andrew Nicholls <andrewtheamazingbrother@gmail.com>
2023-03-09 23:36:37 -0700

    Updating everything after some work

--- login.js
+++ login.js
@@ -1,11 +1,14 @@
 function login() {
     const nameEl = document.querySelector("#form2Example1");
     localStorage.setItem("userName", nameEl.value);
+    document.getElementById("myForm").reset();
+    window.location.reload()
   }
   
 
 const playerNameEl = document.querySelector('.user-name');
 playerNameEl.textContent = this.getPlayerName();
+document.getElementById("myForm").reset();
 
 function getPlayerName() {
   return localStorage.getItem('userName') ?? 'Anonymous';
