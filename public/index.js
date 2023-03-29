d616911aac90f503ace3788a4028a8d929b7c744
Andrew Nicholls <andrewtheamazingbrother@gmail.com>
2023-03-28 23:25:15 -0600

    adding the new stuff

---
+++ public/index.js
@@ -0,0 +1,14 @@
+const searchInputDropdown = document.getElementById('search-input-dropdown');
+const dropdownOptions = document.querySelectorAll('.input-group-dropdown-item');
+
+const accountName = document.querySelector('.user-name');
+accountName.textContent = this.getAccountName();
+
+function getAccountName() {
+  return localStorage.getItem('userName') ?? 'Anonymous';
+}
+
+function goto() {
+    const link = document.querySelector("#classSelection");
+    location.href = link.value;
+  }
---
+++ public/review_maker.js
@@ -0,0 +1,21 @@
+function post() {
+    const gradeEl = document.querySelector("#gradeId");
+    localStorage.setItem("grade", gradeEl.value);
+
+    const classEl = document.querySelector("#classId");
+    localStorage.setItem("class", classEl.value);
+
+    const reviewEl = document.querySelector("#reviewId");
+    localStorage.setItem("review", reviewEl.value);
+
+    document.getElementById("reviewForm").reset();
+    window.location.reload()
+  }
+
+
+const playerNameEl = document.querySelector('.user-name');
+playerNameEl.textContent = this.getPlayerName();
+
+function getPlayerName() {
+  return localStorage.getItem('userName') ?? 'Anonymous';
+}
