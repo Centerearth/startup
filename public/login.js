9f0f1362700d585b0a92dabab4f061e98a3fbaf7
Andrew Nicholls <andrewtheamazingbrother@gmail.com>
2023-04-03 14:19:26 -0600

    Deleting to replace files

--- public/login.js
+++ public/login.js
@@ -1,4 +1,84 @@
-function login() {
+(async () => {
+  let authenticated = false;
+  const userName = localStorage.getItem('userName');
+  if (userName) {
+    const nameEl = document.querySelector('#userName');
+    nameEl.value = userName;
+    const user = await getUser(nameEl.value);
+    authenticated = user?.authenticated;
+  }
+
+  if (authenticated) {
+    document.querySelector('#playerName').textContent = userName;
+    setDisplay('loginControls', 'none');
+    setDisplay('playControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('playControls', 'none');
+  }
+})();
+
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
+}
+
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+  const userName = document.querySelector('#your_name')?.value;
+  const password = document.querySelector('#password')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const body = await response.json();
+
+  if (response?.status === 200) {
+    localStorage.setItem('userName', userName);
+    window.location.href = 'index.html';
+  } else {
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+    document.getElementById("myForm").reset();
+    window.location.reload()
+  }
+}
+
+function logout() {
+  fetch(`/api/auth/logout`, {
+    method: 'delete',
+  }).then(() => (window.location.href = '/'));
+}
+
+async function getUser(email) {
+  let scores = [];
+  // See if we have a user with the given email.
+  const response = await fetch(`/api/user/${email}`);
+  if (response.status === 200) {
+    return response.json();
+  }
+
+  return null;
+}
+
+function setDisplay(controlId, display) {
+  const playControlEl = document.querySelector(`#${controlId}`);
+  if (playControlEl) {
+    playControlEl.style.display = display;
+  }
+}
+
+
+
+
+/*function login() {
     const nameEl = document.querySelector("#form2Example1");
     localStorage.setItem("userName", nameEl.value);
     document.getElementById("myForm").reset();
@@ -11,4 +91,4 @@ accountName.textContent = this.getAccountName();
 
 function getAccountName() {
   return localStorage.getItem('userName') ?? 'Anonymous';
-}
+}*/
