4a48cd4a66fdb5cf040ca5a6e8569d1da056eef7
Andrew Nicholls <andrewtheamazingbrother@gmail.com>
2023-04-03 19:07:16 -0600

    saving work

--- public/reviews.js
+++ public/reviews.js
@@ -1,3 +1,63 @@
+async function loadReviews(classNum) {
+  //need to update each html with proper parameter
+  classNum = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+  alert(classNum);
+  let reviews = [];
+  try {
+    // Get the latest high scores from the service
+    const response = await fetch(`/api/scores/${classNum}`);
+    reviews = await response.json();
+
+    // Save the scores in case we go offline in the future
+    localStorage.setItem('reviews', JSON.stringify(reviews));
+  } catch {
+    // If there was an error then just use the last saved scores
+    const reviewsText = localStorage.getItem('reviews');
+    if (reviewsText) {
+      reviews = JSON.parse(reviewsText);
+    }
+  }
+
+  displayReviews(reviews);
+}
+
+function displayReviews(reviews) {
+  const tableBodyEl = document.querySelector('#reviewTable');
+
+  if (scores.length) {
+    // Update the DOM with the scores
+    for (const [i, review] of reviews.entries()) {
+      const nameTdEl = document.createElement('td');
+      const gradeTdEl = document.createElement('td');
+      const dateTdEl = document.createElement('td');
+      const reviewTdEl = document.createElement('td');
+
+      nameTdEl.textContent = review.name;
+      gradeTdEl.textContent = review.grade;
+      dateTdEl.textContent = review.date;
+      reviewTdEl.textContent = review.name;
+
+      const rowEl = document.createElement('tr');
+      rowEl.appendChild(nameTdEl);
+      rowEl.appendChild(gradeTdEl);
+      rowEl.appendChild(dateTdEl);
+      rowEl.appendChild(reviewTdEl);
+
+      tableBodyEl.appendChild(rowEl);
+    }
+  } else {
+    tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to review!/td></tr>';
+  }
+}
+
+loadReviews();
+
+
+
+
+
+
+/*
 const accountName = document.querySelector('.user-name');
 accountName.textContent = this.getAccountName();
 
@@ -25,3 +85,4 @@ function getClass() {
 function getReview() {
     return localStorage.getItem('review') ?? 'Unknown';
   }
+  */
